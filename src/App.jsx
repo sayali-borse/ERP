95b1e5077b46f44ba7d1225d380bf74eb1bad17a
sayali-borse <sayaliborse91@gmail.com>
2026-04-10 20:44:25 +0530

    Update AuthContext.jsx to centralized data

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,22 @@ import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import Checkout from "./pages/Checkout";
 import Navbar from "./components/Navbar";
+import AuthContext from "./context/AuthContext";
 
 function App() {
   return (
     <>
-      <div className="app">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Auth" element={<Auth />} />
-          <Route path="/Checkout" element={<Checkout />} />
-          <Route path="*" element={<p>404 page is not found</p>} />
-        </Routes>
-      </div>
+      <AuthContext>
+        <div className="app">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Auth" element={<Auth />} />
+            <Route path="/Checkout" element={<Checkout />} />
+            <Route path="*" element={<p>404 page is not found</p>} />
+          </Routes>
+        </div>
+      </AuthContext>
     </>
   );
 }
